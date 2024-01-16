e5ecf0803c64a61696ae18d4ae6e63a5ac8bd14b
scratchCodeMaster <139460148+scratchCodeMaster@users.noreply.github.com>
2024-01-16 18:14:08 +0100

    Create playground.js

---
+++ html_playground/playground.js
@@ -0,0 +1,11 @@
+const $ = (s) => {
+  try { return document.querySelector(s); } catch(err) { return document.createElement(s); }
+};
+
+$.props = {
+  [evt] = (evt, callback, s=null) {
+    if (s === null) s = window;
+    
+    s.addEventListener(evt, callback);
+  }
+};
